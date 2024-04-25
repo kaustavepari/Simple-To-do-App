072168a7fd97a73d36f5883b12a1ef24db84ddb2
Kaustav Epari <kaustavepari@gmail.com>
2024-04-25 20:32:24 +0530

    second commit

--- scripts.js
+++ scripts.js
@@ -1,67 +1,51 @@
+const button = document.getElementById("circle-button")
+const container = document.getElementById("all")
+const inputbox=document.getElementById("input")
+const tasksdisplayer=document.getElementById("counter")
+const para = document.getElementById("paragraph")
 
-
-const inputBox = document.getElementById("input");
-const button = document.getElementById("button");
-const container = document.getElementById("list-container");
-const error=document.createElement("h5")
-const tasksdisplayer = document.createElement("h3")
-tasksdisplayer.innerHTML='Click "Add" to add tasks'
-const clear_button=document.getElementById("Clear")
-
-var has_done_error=false
-card.appendChild(tasksdisplayer)
-var counter = 0
-
-button.addEventListener("click", () => {
-    const newTask = document.createElement("li");
+var current = 0
+var total = 0
+button.addEventListener("click",()=>{
+    const newTask = document.createElement("div")
     
- // Append checkbox to the new task
-
-    // Create a span element to hold the task text
-    const taskText = document.createElement("span");
-    taskText.textContent = inputBox.value;
-    inputBox.value=""
-    if (taskText.textContent==""){
-        
-        error.innerHTML="Task cant be empty string"
-        error.style.color="red"
-        has_done_error=true
-        container.appendChild(error)
-    }else{
-        if(has_done_error==true){
-            has_done_error=false
-            container.removeChild(error)
-        }
-        
-        newTask.appendChild(taskText);
-        taskText.addEventListener("click",()=>{
-            newTask.classList="checked";
-            if(counter>0){
-                counter-=1
-            }
-            if(counter==0){
-                tasksdisplayer.innerHTML="Hurray!!You finished all the tasks"
-            }else{
-            tasksdisplayer.innerHTML=`You have ${counter} tasks left to do`
-            }
-        })
-        container.appendChild(newTask);
-        counter+=1
-        if (counter==1){
-            tasksdisplayer.innerHTML=`You have ${counter} task left to do`
-        } else{
-            tasksdisplayer.innerHTML=`You have ${counter} tasks left to do`
-        }
-        
-        
+    newTask.className="newtask"
+    if (total==0){
+        newTask.style.marginTop="15px"
     }
-     // Append task text to the new task
+    const text = document.createElement("h5")
+    text.style.color="white"
+    text.style.marginLeft="30px"
+    text.style.width="100px"
 
-     // Append the new task to the container
-});
-clear_button.addEventListener("click",()=>{
-    container.innerHTML=""
-    tasksdisplayer.innerHTML='Click "Add" to add tasks'
-    counter=0
+    const circle=document.createElement("div")
+    circle.className="unchecked-circle"
+    newTask.appendChild(circle)
+    if (inputbox.value!=""){
+        text.innerHTML=inputbox.value
+        inputbox.value=""
+    }
+    
+    newTask.addEventListener("click",()=>{
+        if (circle.classList.contains("checked-circle")==false){
+            current+=1
+        }
+        circle.className="checked-circle"
+        text.classList.add("checked")
+        tasksdisplayer.innerHTML=`${current}/${total}`
+        
+    })
+    
+    if (text.innerHTML!=""){
+        total+=1
+        tasksdisplayer.innerHTML=`${current}/${total}`
+        newTask.appendChild(text)
+        container.appendChild(newTask)
+    }
+    
+
+
 })
+
+
 
